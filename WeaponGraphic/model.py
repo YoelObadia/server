67fdc17cd4378e36b702dcb9132bf44f3c9dc1f9
YoelObadia <73312969+YoelObadia@users.noreply.github.com>
2024-04-01 12:59:23 +0300

    Add files via upload

---
+++ WeaponGraphic/model.py
@@ -0,0 +1,10 @@
+class Weapon:
+    def __init__(self, Id, Name, Type, Manufacturer, Caliber, MagazineCapacity, FireRate, AmmoCount):
+        self.Id = Id
+        self.Name = Name
+        self.Type = Type
+        self.Manufacturer = Manufacturer
+        self.Caliber = Caliber
+        self.MagazineCapacity = MagazineCapacity
+        self.FireRate = FireRate
+        self.AmmoCount = AmmoCount
